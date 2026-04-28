654c760fc92cc63af9444403fd0cab87ed27a9d9
Kevin Jeu <kevin.jeu@xperi.com>
2026-04-28 09:10:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v1';
+const CACHE_NAME = 'putseller-v2';
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

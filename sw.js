65e262e0de039bd71f37d134255e4ded54707cf2
Kevin Jeu <kevinjeu@gmail.com>
2026-05-09 12:39:52 -0700

    Bump cache version to putseller-v12

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v11';
+const CACHE_NAME = 'putseller-v12';
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

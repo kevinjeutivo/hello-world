d71b1576856d32cce6759365b0afaf37862cf104
Kevin Jeu <kevinjeu@gmail.com>
2026-05-12 14:41:21 -0700

    Bump cache version from v15 to v16

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v15';
+const CACHE_NAME = 'putseller-v16';
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

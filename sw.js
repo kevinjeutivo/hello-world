71c745dba45a3bb9c8a9b247d8ec860bdfcc6c33
Kevin Jeu <kevinjeu@gmail.com>
2026-05-24 08:18:19 -0700

    Update CACHE_NAME to 'putseller-v74'
    Bump cache version to v74 for new deployment.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v73';
+const CACHE_NAME = 'putseller-v74';
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

d9c587ed5a5a467f2823911174e7505001351b40
Kevin Jeu <kevinjeu@gmail.com>
2026-06-29 18:41:26 -0700

    Bump CACHE_NAME and APP_BUILD to v186
    Updated CACHE_NAME and APP_BUILD for new version.

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v185';
-const APP_BUILD = 185; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v186';
+const APP_BUILD = 186; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

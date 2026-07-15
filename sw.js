da0fd9b60cb35661cff6323c3e679a14a344da3d
Kevin Jeu <kevinjeu@gmail.com>
2026-07-15 02:34:03 -0700

    Bump cache version to v245

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v244';
-const APP_BUILD = 244; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v245';
+const APP_BUILD = 245; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

b6d6a282248c4fba827ed5f83caaa0ad86adbf76
Kevin Jeu <kevinjeu@gmail.com>
2026-08-13 21:42:07 -0700

    Update CACHE_NAME and APP_BUILD in sw.js
    Bump cache version and app build number to 356.

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v354';
-const APP_BUILD = 354; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v356';
+const APP_BUILD = 356; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

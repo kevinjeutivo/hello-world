452d8468757f128af5060f85a88efa2c4eac8757
Kevin Jeu <kevinjeu@gmail.com>
2026-08-20 23:05:44 -0700

    Update CACHE_NAME and APP_BUILD for deployment
    Increment cache version and app build number.

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v378';
-const APP_BUILD = 378; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v379';
+const APP_BUILD = 379; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [

dc418c13dbb7ec02495d8d2fab5d926bd8154e1c
Kevin Jeu <kevinjeu@gmail.com>
2026-09-07 22:08:06 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 // Version bump this string to force a refresh
 // of the cache when you deploy a new version.
 // ============================================
-const CACHE_NAME = 'putseller-v439';
-const APP_BUILD = 439; // increment with every deploy, matches CACHE_NAME version
+const CACHE_NAME = 'putseller-v440';
+const APP_BUILD = 440; // increment with every deploy, matches CACHE_NAME version
 
 // Files to cache on install — the app shell
 const APP_SHELL = [
